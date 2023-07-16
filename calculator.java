29ff3060afd1899ab0791077caa1d0e309b1722d
fujiwara5 <130728457+fujiwara5@users.noreply.github.com>
2023-07-16 20:25:05 +0900

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class sample2{
+  public static void main(String[] args){
+    //Scannerの定義を書いてください。
+
+    System.out.println("plese enter the operator");
+
+    //演算子を読み取ってください。
+
+    System.out.println("plese enter the number");
+
+    //読み取った演算子によって計算をし、出力してください。
+
+  }
+}
